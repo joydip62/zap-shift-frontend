81a1601423427d4018a710aec5807ccb0c4345d2
joydip62 <joydipbanik6@gmail.com>
2026-07-09 01:39:05 +0530

    update folder

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+
+
+
+const Logo = () => {
+    return (
+        <div className='flex items-end'>
+            <img src={logo} alt="" srcset="" />
+            <h3 className='text-3xl font-bold -ms-2.5'>ZapShift</h3>
+        </div>
+    );
+};
+
+export default Logo;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,8 +1,16 @@
 import { createBrowserRouter } from "react-router";
+import RootLayouts from "../layouts/RootLayouts";
+import Home from "../pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world</div>
+      Component: RootLayouts,
+      children:[
+        {
+          index: true,
+          Component: Home
+        }
+      ]
     },
   ]);
